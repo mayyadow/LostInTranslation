75a4a2f714807bdbe744873fce3c3e63b81ee280
Maya Dowman <mayadowman2024@gmail.com>
2025-09-23 12:31:24 -0400

    Update CountryCodeConverter.java

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -57,7 +57,7 @@ public class CountryCodeConverter {
      */
     public String fromCountryCode(String code) {
         // TODO Task B: update this code to use an instance variable to return the correct value
-        return code;
+        return countryToCountryCode.get(code);
     }
 
     /**
