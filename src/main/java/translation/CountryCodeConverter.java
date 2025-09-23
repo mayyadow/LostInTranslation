482fc9b4b25aae9bf4c1d0e723c98fb443d273a5
luciewang <wangyiduo0310@gmail.com>
2025-09-23 12:39:24 -0400

    updated TODO Task B CountryCodeConverter and fromCountryCode

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -69,7 +69,7 @@ public class CountryCodeConverter {
      */
     public String fromCountry(String country) {
         // TODO Task B: update this code to use an instance variable to return the correct value
-        return country;
+        return countryToCountryCode.get(country);
     }
 
     /**
@@ -78,6 +78,6 @@ public class CountryCodeConverter {
      */
     public int getNumCountries() {
         // TODO Task B: update this code to use an instance variable to return the correct value
-        return 0;
+        return countryCodeToCountry.size();
     }
 }
