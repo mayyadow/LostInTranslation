f0d68c1ce9b834edf3704aa8a25f6f2ff9378f46
Xiaohan M. <xiaohan.m9@gmail.com>
2025-09-24 23:39:19 -0400

    Fix LanguageCodeConverter

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,11 +42,13 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                String language_code = line.split("\\s+")[0];
-                String language = line.split("\\s+")[1];
-                languageCodeToLanguage.put(language_code, language);
-                languageToLanguageCode.put(language, language_code);
-                // TODO Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+                if (parts.length >= 2) {
+                    String languageName = parts[0].trim();
+                    String languageCode = parts[1].trim();
+                    languageCodeToLanguage.put(languageCode, languageName);
+                    languageToLanguageCode.put(languageName, languageCode);
+                }
             }
 
         } catch (IOException | URISyntaxException ex) {
